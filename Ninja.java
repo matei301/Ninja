2ea2ebecfaf1c02e0914d629938e14dc4ae01efb
matei301 <mateibalan16@gmail.com>
2014-01-26 00:07:42 +0200

    Create Ninja.java

---
+++ Ninja.java
@@ -0,0 +1,34 @@
+package me.matei.ninja;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test extends JavaPlugin {
+
+	public void onEnable() {
+		Bukkit.getServer().getLogger().info("Test Plugin Enabled!");
+	}
+	
+	public void onDisable() {
+		Bukkit.getServer().getLogger().info("Test Plugin Disabled!");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.AQUA + "The console ran the test command! Good job!");
+			return true;
+		}
+		
+		Player player = (Player) sender;
+		
+		if (cmd.getName().equalsIgnoreCase("test")) {
+			player.sendMessage(ChatColor.GOLD + "You ran the test command! Good job!");
+		}
+		return true;
+	}
+}
